1158a83351913be8237235f0fcc7688586af510e
Gabriele Santomaggio <gsantomaggio@suse.com>
2020-02-13 13:24:51 +0100

    WIP

--- istio_lb_tcp/istio.sh
+++ istio_lb_tcp/istio.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-eval $(minikube docker-env)
-kubectl apply  -f <(istioctl kube-inject -f deploy/book.yaml)
+#eval $(minikube docker-env)
+kubectl apply  -f <(istioctl kube-inject -f deploy/rabbitmq.yaml)
 #kubectl apply  -f <(istioctl kube-inject -f deploy/books_v2.yaml)
 kubectl apply  -f deploy/gateway.yaml
 kubectl apply  -f deploy/route.yaml
+kubectl apply -f deploy/ingress.yaml
+kubectl apply  -f <(istioctl kube-inject -f deploy/ingress.yaml)
+#kubectl apply  -f <(istioctl kube-inject -f deploy/ingress.yaml)
