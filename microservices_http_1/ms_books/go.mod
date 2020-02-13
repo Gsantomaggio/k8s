79e4c1f32a8bcb1c8c880f56b4348ba0875be7ed
Gabriele Santomaggio <gsantomaggio@suse.com>
2020-02-13 20:53:02 +0100

    start

--- microservices_http_1/ms_books/go.mod
+++ microservices_http_1/ms_books/go.mod
@@ -2,4 +2,4 @@ module books
 
 go 1.13
 
-require github.com/prometheus/client_golang v1.4.0 // indirect
+require github.com/prometheus/client_golang v1.4.0
--- microservices_http_1/ms_books/main.go
+++ microservices_http_1/ms_books/main.go
@@ -3,9 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
 	"os"
@@ -28,13 +25,6 @@ type Host struct {
 func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
-}
-
-func itemsHandler(w http.ResponseWriter, r *http.Request) {
-	recordMetrics()
-	log.Printf("[store] called the books API")
-	getBooks(w)
-
 }
 
 func getBooks(w http.ResponseWriter) {
@@ -64,29 +54,5 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/items", itemsHandler)
 	log.Printf("[store] books is started")
-
-	mux.HandleFunc("/healthz", healthHandler)
-	go startProm()
 	http.ListenAndServe(":"+port, mux)
-
 }
-
-func startProm() {
-	log.Printf("[store] books- prom is starting...")
-	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":2112", nil)
-
-}
-
-func recordMetrics() {
-	go func() {
-		opsProcessed.Inc()
-	}()
-}
-
-var (
-	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "books_number_of_get_items",
-		Help: "The total number HTTP get books items",
-	})
-)
