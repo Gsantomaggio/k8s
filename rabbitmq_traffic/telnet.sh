58db4c5b61278c11cd203d614e627e0a727ca5f4
Gabriele Santomaggio <gsantomaggio@suse.com>
2020-03-04 18:32:35 +0100

    add ingress

--- rabbitmq_traffic/telnet.sh
+++ rabbitmq_traffic/telnet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export INGRESS_PORT=$(kubectl -n istio-system get service custom-gateway -o jsonpath='{.spec.ports[?(@.name=="amqp")].port}')
-export INGRESS_HOST=$(kubectl -n istio-system get service custom-gateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+export INGRESS_PORT=$(kubectl -n istio-system get service rabbitmq-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="amqp")].port}')
+export INGRESS_HOST=$(kubectl -n istio-system get service rabbitmq-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 echo $INGRESS_HOST:$INGRESS_PORT
 
 #telnet $INGRESS_HOST 31400
