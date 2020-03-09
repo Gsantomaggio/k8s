71e3bc00e590f1580b693fc2cd93f1b348e21e22
Gabriele Santomaggio <gsantomaggio@suse.com>
2020-03-09 18:08:00 +0100

    Add prod stage example

--- prod_stage/web_app/main.go
+++ prod_stage/web_app/main.go
@@ -1,1 +1,31 @@
-package web_app
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	host, _ :=   os.Hostname()
+	host =  fmt.Sprintf("<h1>{hostname: %s}</h1>", host)
+	env := fmt.Sprintf("<h1>{Test page for: %s}</h1>", getEnv() )
+	w.Write([]byte(host))
+	w.Write([]byte(env))
+}
+
+
+func getEnv() string {
+	return os.Getenv("ENVIRONMENT")
+}
+
+
+func main() {
+	log.Printf("[webapp]  is starting...")
+	port := "2000"
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", indexHandler)
+	log.Printf("[webapp] is started")
+	http.ListenAndServe(":"+port, mux)
+}
