462d30b2027d70c360e3e9c0cd578e1c9ce99651
Gabriele Santomaggio <g.santomaggio@gmail.com>
2020-11-24 11:39:45 +0100

    example

--- microservices_http_1/ms_books/main.go
+++ microservices_http_1/ms_books/main.go
@@ -3,9 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"log"
 	"net/http"
 	"os"
@@ -31,7 +28,6 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func itemsHandler(w http.ResponseWriter, r *http.Request) {
-	recordMetrics()
 	log.Printf("[store] called the books API")
 	getBooks(w)
 
@@ -42,10 +38,6 @@ func getBooks(w http.ResponseWriter) {
 	host, _ := os.Hostname()
 	host = fmt.Sprintf("{hostname: %s}", host)
 	books.HostName = host
-	book := &Book{}
-
-	book.Title = "Hello"
-	book.Author = "John Doe"
 
 	books.Books = append(books.Books, &Book{Title: "Golang Programming", Author: "John Doe"})
 	books.Books = append(books.Books, &Book{Title: "Kubernetes Programming", Author: "Alex Kubernetes"})
@@ -62,31 +54,8 @@ func main() {
 	log.Printf("[store] books is starting...")
 	port := "2200"
 	mux := http.NewServeMux()
-	mux.HandleFunc("/items", itemsHandler)
-	log.Printf("[store] books is started")
-
-	mux.HandleFunc("/healthz", healthHandler)
-	go startProm()
+	mux.HandleFunc("/", itemsHandler)
+	log.Printf("[store] books is started: " + "http://localhost:2200/")
 	http.ListenAndServe(":"+port, mux)
-
 }
 
-func startProm() {
-	log.Printf("[store] books- prom is starting...")
-	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":2112", nil)
-
-}
-
-func recordMetrics() {
-	go func() {
-		opsProcessed.Inc()
-	}()
-}
-
-var (
-	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "books_number_of_get_items",
-		Help: "The total number HTTP get books items",
-	})
-)
---
+++ microservices_http_1/ms_books/start.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+minikube start --cpus=2 --memory=4g
